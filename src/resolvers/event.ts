ed1e216cca44aff21326e014e684ec6b5dc1752f
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-10 12:36:39 +1000

    include host relation in posts

--- src/resolvers/event.ts
+++ src/resolvers/event.ts
@@ -44,7 +44,7 @@ export class EventResolver {
     // @Arg("limit", () => Int) limit: number,
     // @Arg("cursor", () => String, { nullable: true }) cursor: string | null
 
-    return Event.find({});
+    return Event.find({ relations: ["host"] });
 
     // const realLimit = Math.min(50, limit);
     // const realLimitPlusOne = realLimit + 1;
